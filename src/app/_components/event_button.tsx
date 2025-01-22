a44a8b7f21cb6b371a96b01b993580289e4ec600
buyna-gtn <b.bekhbayar@gtn.co.jp>
2025-01-22 09:03:40 +0800

    feat: add second section

--- src/app/_components/event_button.tsx
+++ src/app/_components/event_button.tsx
@@ -1,12 +1,12 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-import { sendGTMEvent } from "@next/third-parties/google";
+// import { sendGTMEvent } from "@next/third-parties/google";
 
 export const EventButton: React.FC = () => {
   return (
     <Button
-      onClick={() => sendGTMEvent({ event: "buttonClicked", value: "xyz" })}
+      // onClick={() => sendGTMEvent({ event: "buttonClicked", value: "xyz" })}
       size="lg"
       id="event-button"
       className="text-xl font-semibold event-button"
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,17 @@
-import { EventButton } from "./_components/event_buttont";
+import { EventButton } from "./_components/event_button";
 
 const HomePage: React.FC = () => {
   return (
-    <div className="min-h-screen flex items-center justify-center flex-col gap-y-8">
-      <h1 className="text-4xl font-bold">Google Tag Manager</h1>
-      <EventButton />
-    </div>
+    <main>
+      <div className="min-h-screen flex items-center justify-center flex-col gap-y-8">
+        <h2 className="text-4xl font-bold">Google Tag Manager</h2>
+        <EventButton />
+      </div>
+      <div className="min-h-screen flex items-center justify-center flex-col gap-y-8">
+        <h2 className="text-4xl font-bold">Google Tag Manager</h2>
+        <EventButton />
+      </div>
+    </main>
   );
 };
 export default HomePage;
